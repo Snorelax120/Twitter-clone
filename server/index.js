c87b77b2090763bbbca15de2cef3075271f080ea
Ritwik Roy <roy.ritwik12@gmail.com>
2023-12-03 18:43:29 -0500

    Added enviroment variables to connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose"
+
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.send("Hello!!");
-})
+dotenv.config();
+
+const connect = () =>{
+    mongoose.connect(process.env.MONGO)
+    .then(()=> {
+        console.log("Connected to mongodb database");
+    })
+    .catch((err)=>{
+        throw err;
+    });
+
+}
 
 app.listen(8000, () => {
+    connect();
     console.log("Listening to port 8000");
 })
